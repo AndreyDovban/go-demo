8b90a4fe3c21722291b6237532a8a0ea47750cc1
Andrey Dov <dovban.andreyy@rambler.ru>
2024-07-24 22:45:52 +0300

    перменные и типы

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print("Hello\n")
+	const IMTPower = 2
+	var userHeight = 1.57
+	var userKg float64 = 54
+	var IMT = userKg / math.Pow(userHeight, IMTPower)
+
+	fmt.Print(IMT)
 }
