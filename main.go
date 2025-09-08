1724b2eb9ab72f697cbf6c4b2e0e4cc549d093d2
Довбан Андрей Сергеевич <dovban@mdis.ru>
2025-09-08 11:42:28 +0300

    fix

--- main.go
+++ main.go
@@ -6,16 +6,34 @@ import (
 )
 
 func main() {
+
+	userHeight, userKg := getUserInput()
+
+	var IMT = calculateIMT(userKg, userHeight)
+
+	outputResult(IMT)
+
+}
+
+func outputResult(imt float64) {
+	result := fmt.Sprintf("Index body mass: %.2f", imt)
+	fmt.Println(result)
+}
+
+func calculateIMT(weight, height float64) float64 {
 	const IMTPower = 2
-	var userHeight float64
-	var userKg float64
+
+	return weight / math.Pow(height, IMTPower)
+}
+
+func getUserInput() (float64, float64) {
+	var height float64
+	var weight float64
 
 	fmt.Println("Enter you height:")
-	fmt.Scan(&userHeight)
+	fmt.Scan(&height)
 	fmt.Println("Enter you weihgt:")
-	fmt.Scan(&userKg)
+	fmt.Scan(&weight)
 
-	var IMT = userKg / math.Pow(userHeight, IMTPower)
-
-	fmt.Printf("Index body mass: %.2f", IMT)
+	return height, weight
 }
